afe00a588f57cf3d2b06ac7392d1c9cd8c645f4f
Alberto Carrillo <beto.valo59@gmail.com>
2025-07-01 09:58:14 -0600

    separar tests de videosSlice en archivos por acción

--- src/features/videos/__test__/fetchVideos.test.js
+++ src/features/videos/__test__/fetchVideos.test.js
@@ -1,32 +1,8 @@
-import reducer, { toggleLike } from "./videosSlice"
-import { getBaseVideoState ,getVideoStateWithLikes } from "./helpers/stateHelpers"
-import { fetchVideos } from "./videosSlice";
-// TEST: si el "like" está inicialmente en false, al alternarlo debería pasar a true
-test('should toggle like from false to true', () => {
+import reducer from "../videosSlice";
+import { getBaseVideoState } from "../helpers/stateHelpers";
+import { fetchVideos } from "../videosSlice";
 
-    const initialState = getVideoStateWithLikes({ '1': false }) // Preparar: estado inicial con like en false para el video con ID '1'
 
-    const newState = reducer(initialState, toggleLike('1')) // Actuar: se despacha la acción toggleLike
-
-    expect(newState.likes['1']).toBe(true)  // Verificar: el like ahora debería ser true
-});
-// TEST: si el "like" está inicialmente en true, al alternarlo debería pasar a false
-test('should toggle like from true to false', () => {
-
-    const initialState = getVideoStateWithLikes({ '1': true })  // Preparar: estado inicial con like en true para el video con ID '1'
-
-    const newState = reducer(initialState, toggleLike('1')) // Actuar: se despacha la acción toggleLike
-
-    expect(newState.likes['1']).toBe(false) // Verificar: el like ahora debería ser false
-});
-// TEST: si el "like" no existe (es undefined), al alternarlo debería establecerse en true
-test('should toggle like from undefined to true', () => {
-    const initialState = getVideoStateWithLikes() // Preparar: estado inicial sin ningún like definido
-
-    const newState = reducer(initialState, toggleLike('999'))   // Actuar: se despacha la acción toggleLike sobre el video con ID '999'
-
-    expect(newState.likes['999']).toBe(true)    // Verificar: el like del video '999' debería ser true
-});
 //  TEST: Verifica que el estado cambie a "loading" cuando se inicia la carga de videos
 test('should handle fetchVideos.pending and set status to loading', () => {
     const initialState = getBaseVideoState()    //  Estado inicial vacío
---
+++ src/features/videos/__test__/toggleLike.test.js
@@ -0,0 +1,28 @@
+import reducer, { toggleLike } from "../videosSlice"
+import { getVideoStateWithLikes } from "../helpers/stateHelpers"
+// TEST: si el "like" está inicialmente en false, al alternarlo debería pasar a true
+test('should toggle like from false to true', () => {
+
+    const initialState = getVideoStateWithLikes({ '1': false }) // Preparar: estado inicial con like en false para el video con ID '1'
+
+    const newState = reducer(initialState, toggleLike('1')) // Actuar: se despacha la acción toggleLike
+
+    expect(newState.likes['1']).toBe(true)  // Verificar: el like ahora debería ser true
+});
+// TEST: si el "like" está inicialmente en true, al alternarlo debería pasar a false
+test('should toggle like from true to false', () => {
+
+    const initialState = getVideoStateWithLikes({ '1': true })  // Preparar: estado inicial con like en true para el video con ID '1'
+
+    const newState = reducer(initialState, toggleLike('1')) // Actuar: se despacha la acción toggleLike
+
+    expect(newState.likes['1']).toBe(false) // Verificar: el like ahora debería ser false
+});
+// TEST: si el "like" no existe (es undefined), al alternarlo debería establecerse en true
+test('should toggle like from undefined to true', () => {
+    const initialState = getVideoStateWithLikes() // Preparar: estado inicial sin ningún like definido
+
+    const newState = reducer(initialState, toggleLike('999'))   // Actuar: se despacha la acción toggleLike sobre el video con ID '999'
+
+    expect(newState.likes['999']).toBe(true)    // Verificar: el like del video '999' debería ser true
+});
